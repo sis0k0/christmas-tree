c5a5637a47d1b9811b181e67f55203a00bd2e845
sis0k0 <stanimira.vlaeva@gmail.com>
2019-01-23 13:53:22 +0200

    add HMR plugin only in development mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,23 @@
 const path = require('path');
 const webpack = require('webpack');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  devServer: {
-    contentBase: './dist',
-    hot: true,
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ],
+module.exports = (env, argv) => {
+    const config = {
+        entry: './src/index.js',
+        output: {
+            filename: 'main.js',
+            path: path.resolve(__dirname, 'dist')
+        },
+        devServer: {
+            contentBase: './dist',
+            hot: true,
+        },
+        plugins: [],
+    };
+
+    if (argv.mode === 'development') {
+        config.plugins.push(new webpack.HotModuleReplacementPlugin());
+    }
+
+    return config;
 };
