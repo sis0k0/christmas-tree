175c77ebbc3b4508bb9d5dc36b8c1fb291f80670
叶明巧宇 <wb-ymqy816695@antgroup.com>
2022-11-07 01:24:46 +0800

    feat: webpack v5

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,19 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
 
 module.exports = (env, argv) => {
-    const config = {
-        entry: './src/index.js',
-        output: {
-            filename: 'main.js',
-            path: path.resolve(__dirname, 'dist')
-        },
-        devServer: {
-            contentBase: './dist',
-            hot: true,
-        },
-        plugins: [],
-    };
+  const config = {
+    entry: "./src/index.js",
+    output: {
+      filename: "main.js",
+      path: path.resolve(__dirname, "dist"),
+    },
+    devServer: {
+      static: {
+        directory: path.join(__dirname, "./dist"),
+      },
+    },
+    plugins: [],
+  };
 
-    if (argv.mode === 'development') {
-        config.plugins.push(new webpack.HotModuleReplacementPlugin());
-    }
-
-    return config;
+  return config;
 };
