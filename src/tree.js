81940050856a0b461ee89dac3cff14d35db636bf
叶明巧宇 <wb-ymqy816695@antgroup.com>
2022-11-07 01:12:10 +0800

    style: prettier format

--- src/tree.js
+++ src/tree.js
@@ -2,29 +2,29 @@
 //     module.hot.accept();
 // }
 
-const NEEDLE_CLASS = 'needle';
+const NEEDLE_CLASS = "needle";
 
 const fir = {
-    container: document.getElementById('tree'),
-    getNeedles: function() {
-        return this.container.getElementsByClassName(NEEDLE_CLASS);
-    },
-    rowsCount: 20,
-    draw: function() {
-        let output = '';
+  container: document.getElementById("tree"),
+  getNeedles: function () {
+    return this.container.getElementsByClassName(NEEDLE_CLASS);
+  },
+  rowsCount: 20,
+  draw: function () {
+    let output = "";
 
-        for (let i = 1; i < this.rowsCount; i += 1) {
-            for (let j = i; j < this.rowsCount; j += 1) {
-                output += '&nbsp;&nbsp;';
-            }
-            for (let k = 0; k <= i * 2; k += 1) {
-                output += `<span class="${NEEDLE_CLASS}">1</span>`;
-            }
-            output += '<br />';
-        }
-
-        this.container.innerHTML = output;
+    for (let i = 1; i < this.rowsCount; i += 1) {
+      for (let j = i; j < this.rowsCount; j += 1) {
+        output += "&nbsp;&nbsp;";
+      }
+      for (let k = 0; k <= i * 2; k += 1) {
+        output += `<span class="${NEEDLE_CLASS}">1</span>`;
+      }
+      output += "<br />";
     }
+
+    this.container.innerHTML = output;
+  },
 };
 
 fir.draw();
